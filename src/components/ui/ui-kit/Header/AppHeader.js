f352bf1c447f3b0bd6c7ddb3fbdfc7877ccd9c1d
Praveen Ramachandran <praveen.r@diagnal.com>
2019-02-25 18:50:44 +0530

    Created AppHeader component which imports and uses AppLogo, SearchBar and SearchIcon components. Added example to show in styleguide.

---
+++ src/components/ui/ui-kit/Header/AppHeader.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import AppLogo from './AppLogo';
+import SearchBar from './SearchBar';
+import SearchIcon from './SearchIcon';
+
+class AppHeader extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchbarOpen: false
+    }
+  }
+
+  handleSearchBarToggle = () => {
+    this.setState({
+      searchbarOpen: !this.state.searchbarOpen
+    })
+  }
+
+  render () {
+    return (
+      <header className="app-header w-full h-72 w-full bg-transparent flex flex-no-wrap items-center justify-center overflow-hidden">
+        <div className="app-logo-wrap flex-none">
+          <AppLogo />
+        </div>
+
+        <div className="searchbar-wrap flex-1" />
+
+        <div className="searchbar-toggle-wrap flex-none">
+          <button className="searchbar-toggle-button outline-none focus:outline-none p-2" onClick={() => this.handleSearchBarToggle()}>
+            <SearchIcon />
+          </button>
+        </div>
+      </header>
+    )
+  }
+}
+
+AppHeader.propTypes = {
+  /** Text to display as logo hover tooltip */
+  logoText: PropTypes.string.isRequired,
+  /** Default or compact version */
+  compact: PropTypes.bool.isRequired
+};
+
+AppHeader.defaultProps = {
+  logoText: '',
+  compact: false
+};
+
+export default AppHeader;
---
+++ src/components/ui/ui-kit/Header/AppHeader.md
@@ -0,0 +1,7 @@
+AppHeader:
+
+```js
+<div className="bg-black p-6 flex flex-row">
+  <AppHeader logoText="Diagnal" />
+</div>
+```
