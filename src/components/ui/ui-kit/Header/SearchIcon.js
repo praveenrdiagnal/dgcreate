1754b13947fa85911a14dfe739d32f79757eae25
Praveen Ramachandran <praveen.r@diagnal.com>
2019-02-25 16:46:51 +0530

    Created SearchIcon component and added example for styleguide.

---
+++ src/components/ui/ui-kit/Header/SearchIcon.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchIcon extends Component {
+  render () {
+    return (
+      <div className="search-icon h-72 w-72" />
+    )
+  }
+}
+
+SearchIcon.propTypes = {
+};
+
+SearchIcon.defaultProps = {
+};
+
+export default SearchIcon;
---
+++ src/components/ui/ui-kit/Header/SearchIcon.md
@@ -0,0 +1,7 @@
+SearchIcon:
+
+```js
+<div className="bg-black p-6 flex flex-row">
+  <SearchIcon />
+</div>
+```
