5024176caef77bf4948bf7f59e0f44a006768fb6
Praveen Ramachandran <praveen.r@diagnal.com>
2019-02-27 11:33:36 +0530

    Installed and setup postcss-scss plugin to work with scss files within tailwind.

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 const tailwindcss = require('tailwindcss');
 module.exports = {
+    syntax: 'postcss-scss',
+    parser: 'postcss-scss',
     plugins: [
         tailwindcss('./tailwind.config.js'),
         require('autoprefixer'),
